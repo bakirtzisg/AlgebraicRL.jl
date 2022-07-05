ad871ed4edce670682cf17957cd318f3828b37aa
undergrad <undergrad@asg-a32252.oden.utexas.edu>
2022-07-05 14:32:13 -0500

    Adding docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, ReinforcementLearning, AlgebraicDynamics, AlgebraicDynamics.DWDDynam, AlgebraicRL
 
-makedocs(sitename="Algebraic RL")
+makedocs(sitename="AlgebraicRL.JL")
 
 deploydocs(repo = "github.com/bakirtzisg/AlgebraicRL.jl.git",)
